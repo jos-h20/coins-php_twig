634b1c09f2385104fbc7dbeb545be63d95c0712b
Josh Overly and Ronando Long <ronando.long@gmail.com>
2016-02-16 15:34:45 -0800

    It renders twig home page.

--- app/app.php
+++ app/app.php
@@ -1,17 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/CoinCombo.php";
-        // If you hit the BSOD (blank screen of death) in a Silex app, add these lines to your app.php file right under your require_once statements.
-        //Add symfony debug component and turn it on.
-            //use Symfony\Component\Debug\Debug;
-            //Debug::enable();
-
-        // Set Silex debug mode in $app object
-            //$app['debug'] = true;
-    session_start();
-    if (empty($_SESSION['list_of_places'])) {
-        $_SESSION['list_of_places'] = array();
-    }
 
     $app = new Silex\Application();
 
@@ -23,8 +12,10 @@
 
     // Render Home Page
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('example.html.twig'); //
+        return $app['twig']->render('coins.html.twig'); //
     });
+
+    
 
     return $app;
 
