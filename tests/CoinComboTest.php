f1880b7dc603584d7a89b62aae6b0c06513f9815
Josh Overly and Ronando Long <ronando.long@gmail.com>
2016-02-16 15:20:12 -0800

    Spec Fail: Fails to push quarters to array.

--- tests/CoinComboTest.php
+++ tests/CoinComboTest.php
@@ -18,31 +18,49 @@
             $this->assertEquals([0, 0, 0, 1], $result);
         }
 
+        // Nickles
         function test_CoinCombo_pennyAndNickle()
         {
             //Arrange
             $test_CoinCombo = new CoinCombo;
             $input = 7;
 
-            //Act: runs the actual method that we are testing.
+            //Act
             $result = $test_CoinCombo->makeChange($input);
 
-            //Assert tells our tests what to expect from the output of our method.
+            //Assert
             $this->assertEquals([0, 0, 1, 2], $result);
         }
 
+        // Dimes
         function test_CoinCombo_pennyNickleDime()
         {
             //Arrange
             $test_CoinCombo = new CoinCombo;
             $input = 17;
 
-            //Act: runs the actual method that we are testing.
+            //Act
             $result = $test_CoinCombo->makeChange($input);
 
-            //Assert tells our tests what to expect from the output of our method.
+            //Assert
             $this->assertEquals([0, 1, 1, 2], $result);
         }
+
+        // Quarters
+        function test_CoinCombo_pennyNickleDimeQrtrs()
+        {
+            //Arrange
+            $test_CoinCombo = new CoinCombo;
+            $input = 42;
+
+            //Act
+            $result = $test_CoinCombo->makeChange($input);
+
+            //Assert
+            $this->assertEquals([1, 1, 1, 2], $result);
+        }
+
+
     }
 
     // Run in terminal in project folder
