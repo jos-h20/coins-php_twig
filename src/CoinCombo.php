264b5edda69da92f137e6c0e8343beb962b3b7e6
Josh Overly and Ronando Long <joshoverly@yahoo.com>
2016-02-16 15:12:30 -0800

    Spec Passes: It will push pennies and nickles to an array.

--- src/CoinCombo.php
+++ src/CoinCombo.php
@@ -4,9 +4,14 @@
 
         function makeChange($input_amount)
         {
-            $coinage = [0,0,0,0];
+            $coinage = [0, 0, 0, 0];
 
-            $coinage[3] = $input_amount;
+            $nickles = floor($input_amount / 5);
+            $coinage[2] = $nickles;
+            $remainder = ($input_amount % 5);
+
+            $pennies = $remainder;
+            $coinage[3] = $pennies;
 
             return $coinage;
         }
